4c48f71fe0bd2c8254c4f181148076a0f929ec83
samvishniya <sam.vishniya@gmail.com>
2019-10-25 20:41:11 +0100

    Added Java EndOfFile

---
+++ Introduction/src/Solution.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+
+		int lineCounter =0;
+		while (in.hasNext()) {
+			lineCounter++;
+			String a = in.nextLine();
+			System.out.println(lineCounter+" " +a);
+		}
+
+		/*
+		 * Read input from STDIN (std input). Print output to STDOUT. Your class should
+		 * be named Solution.
+		 */
+	}
+}
